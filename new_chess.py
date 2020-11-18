9a1d7925b62e58e2e3ed45614454a29707aa4fca
ngoalvin <74577614+ngoalvin@users.noreply.github.com>
2020-11-18 14:28:50 -0500

    Update new_chess.py

--- new_chess.py
+++ new_chess.py
@@ -1,6 +1,8 @@
 class Chess:
     def __init__(self):
         self.board = [[None for _ in range(8)] for _ in range(8)]
+        self.history = {}
+        self.move_counter = 1
     
     def add_piece(self, piece):
         x = piece.get_x()
@@ -17,7 +19,25 @@ class Chess:
             for y in x:
                 if type(y) is King:
                     king_count += 1
-        return king_count == 2 
+        return king_count == 2
+    def add_move(self):
+        if self.move_counter not in self.history:
+            self.history.update({self.move_counter:self.board})
+            self.move_counter += 1
+            
+    def replay_turn(self, turn):
+        print (dic[turn])
+            
+        
+    
+    def pprint(self):
+        for i in range(8):
+            for j in range(8):
+                if not self.board[i][j]:
+                    print("-", end=" ")
+                else:
+                    print(self.board[i][j].piece, end=" ")
+            print()
                                      
 class Piece:
     def __init__ (self, colour:str, x:int, y:int, piece_type:str):
@@ -51,32 +71,33 @@ class Piece:
     def get_y(self):
         return self.y
     
-    def is_start_position(self, new_x:int, new_y:int):
+    def is_start_position(self):
         #checks if the piece is in the start position
-        return self.start_x == new_x and self.start_y == new_y
+        return self.start_x == self.x and self.start_y == self.y
     
     def is_ally(self, new_x:int, new_y:int, board):
         #check if a piece is an ally
-        return board[self.x][self.y].colour == board[new_x][new_y].colour
-    
+        if board[new_x][new_y]:
+            return board[self.x][self.y].colour == board[new_x][new_y].colour
+        return False
     
     def can_kill(self, new_x, new_y, board):
         #checks if you can take a piece
         return not self.is_ally(new_x, new_y, board)
     
-    def all_moves(self,board):
+    def all_moves(self, board):
         lst = []
         for x in range(8):
             for y in range(8):
                 if self.move_is_valid(x, y, board):
                     lst.append([x,y])
-        print(lst)
-        return lst
+        return lst      
+    
                     
     def check(self,board):
         moves = self.all_moves(board)
         for move in moves:
-            if board[move[0]][move[1]] != None:
+            if board[move[0]][move[1]] is not None:
                 if 'KING' in board[move[0]][move[1]].piece:
                     if self.can_kill(move[0], move[1]):
                         return True
@@ -110,24 +131,24 @@ class Rook(Piece):
         step_y = -1 if is_moving_down else 1
         x = self.x
         y = self.y
-        if (0 <= new_x < 8) and (0 <= new_y < 8):#checks if valid x , y
-            if (self.x == new_x and self.y != new_y):#checks if moving vertical
-                while x == new_x and y != new_y: 
-                    y += step_y
-                    if board[x][y] is not None:#checks if theres a piece blocking in between locations
-                        return False
-                if board[new_x][new_y] is not None:#checks if the position has a piece
-                    return can_kill(new_x, new_y, board)#checks if you can take the piece
-                return True
-            if (self.x == new_x and self.y != new_y):
-                while x != new_x and y == new_y: 
-                    x += step_x
-                    if board[x][y] is not None:
-                        return False
-                if board[new_x][new_y] is not None:
-                    return can_kill(new_x, new_y, board)
-                return True
-        return False
+        if not (0 <= new_x < 8) and (0 <= new_y < 8):
+            return False#checks if valid x , y
+        if (self.x == new_x and self.y != new_y):#checks if moving vertical
+            while x == new_x and y != new_y: 
+                y += step_y
+                if board[x][y] is not None:#checks if theres a piece blocking in between locations
+                    return False
+            if board[new_x][new_y] is not None:#checks if the position has a piece
+                return can_kill(new_x, new_y, board)#checks if you can take the piece
+            return True
+        if (self.x != new_x and self.y == new_y):
+            while x != new_x and y == new_y: 
+                x += step_x
+                if board[x][y] is not None:
+                    return False
+            if board[new_x][new_y] is not None:
+                return can_kill(new_x, new_y, board)
+            return True
 
 class Queen(Piece):
     def __init__(self, colour:str, x:int, y:int):
@@ -142,33 +163,33 @@ class Queen(Piece):
         step_y = -1 if is_moving_down else 1
         x = self.x
         y = self.y
-        if (0 <= new_x < 8) and (0 <= new_y < 8):
-            if (abs(self.x - new_x)) == abs(self.y - new_y):#check if its a diagonal move
-                while x != (new_x - step_x) and y != (new_y - step_y):
-                    x += step_x
-                    y += step_y
-                    if board[x][y] is not None:
-                        return False
-                if board[new_x][new_y] is not None:
-                    return self.can_kill(new_x, new_y, board)
-                return True
-            if (self.x == new_x and self.y != new_y):
-                while y != (new_y - step_y):
-                    y += step_y
-                    if board[x][y] is not None:
-                        return False
-                if board[new_x][new_y] is not None:
-                    return self.can_kill(new_x, new_y, board)
-                return True
-            if (self.x != new_x and self.y == new_y):
-                while x != (new_x - step_x):
-                    x += step_x
-                    if board[x][y] is not None:
-                        return False
-                if board[new_x][new_y] is not None:
-                    return self.can_kill(new_x, new_y, board)
-                return True
-        return False
+        if not (0 <= new_x < 8) and (0 <= new_y < 8):
+            return False
+        if (abs(self.x - new_x)) == abs(self.y - new_y):#check if its a diagonal move
+            while x != (new_x - step_x) and y != (new_y - step_y):
+                x += step_x
+                y += step_y
+                if board[x][y] is not None:
+                    return False
+            if board[new_x][new_y] is not None:
+                return self.can_kill(new_x, new_y, board)
+            return True
+        if (self.x == new_x and self.y != new_y):
+            while y != (new_y - step_y):
+                y += step_y
+                if board[x][y] is not None:
+                    return False
+            if board[new_x][new_y] is not None:
+                return self.can_kill(new_x, new_y, board)
+            return True
+        if (self.x != new_x and self.y == new_y):
+            while x != (new_x - step_x):
+                x += step_x
+                if board[x][y] is not None:
+                    return False
+            if board[new_x][new_y] is not None:
+                return self.can_kill(new_x, new_y, board)
+            return True
           
     
 class Knight(Piece):
@@ -178,16 +199,17 @@ class Knight(Piece):
         self.start_y = y   
         
     def move_is_valid(self, new_x:int, new_y:int, board):
-        if ((0 <= new_x < 8) and (0 <= new_y < 8)):
-            if abs(self.x - new_x) == 1 and abs(self.y - new_y) == 2:
-                if board[new_x][new_y] is not None:
-                    return self.can_kill(new_x, new_y, board) 
-                return True
-            if abs(self.x - new_x) == 2 and abs (self.y - new_y) == 1:
-                if board[new_x][new_y] is not None:
-                    return self.can_kill(new_x, new_y, board) 
-                return True
-        return False
+        if not ((0 <= new_x < 8) and (0 <= new_y < 8)):
+            return False
+        if abs(self.x - new_x) == 1 and abs(self.y - new_y) == 2:
+            if board[new_x][new_y] is not None:
+                return self.can_kill(new_x, new_y, board) 
+            return True
+        if abs(self.x - new_x) == 2 and abs (self.y - new_y) == 1:
+            if board[new_x][new_y] is not None:
+                return self.can_kill(new_x, new_y, board) 
+            return True
+        
 
 class Bishop(Piece):
     def __init__(self, colour:str, x:int, y:int):
@@ -225,39 +247,42 @@ class Pawn(Piece):
         step_y = -1 if is_moving_down else 1
         x = self.x
         y = self.y
-        if (0 <= new_x < 8) and (0 <= new_y < 8):
-            while x == new_x and y != (new_y - step_y):
-                y += step_y
-                if board[x][y] is not None:
-                    return False
-                
-            if self.is_start_position(self.x, self.y): #checks if pawn hasnt move
-                if board[self.x][self.y].colour == 'W':#checks if its white or black                  
-                    if abs(self.y - new_y) <= 2 and self.x == new_x and not is_moving_down:#if it is white make sure its not moving down
-                        return True
-                    if abs(self.y- new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):#if it is black make sure its moving down
-                        return self.can_kill(new_x, new_y, board)                    
-                else:
-                    if abs(self.y - new_y) <= 2 and self.x == new_x and is_moving_down:
-                        return True
-                    if abs(self.y- new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):
-                        return self.can_kill(new_x, new_y, board)                     
+        if not (0 <= new_x < 8) and (0 <= new_y < 8):
+            return False
+        while x == new_x and y != (new_y - step_y):
+            y += step_y
+            if board[x][y] is not None:
+                return False
             
-            
-            if board[self.x][self.y].colour == 'W':               
-                if abs(self.y - new_y) < 2 and self.x == new_x and not is_moving_down:
+        if self.is_start_position(): #checks if pawn hasnt move
+            if board[self.x][self.y].colour == 'W':#checks if its white or black                  
+                if abs(self.y - new_y) <= 2 and self.x == new_x and not is_moving_down:#if it is white make sure its not moving down
                     return True
-                if abs(self.y- new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):
-                    return self.can_kill(new_x, new_y, board)
+                if abs(self.y - new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):
+                    if board[new_x][new_y]:
+                        return self.can_kill(new_x, new_y, board)
+                        
             else:
-                if abs(self.y - new_y) < 2 and self.x == new_x and is_moving_down:
+                if abs(self.y - new_y) <= 2 and self.x == new_x and is_moving_down:#if it is black make sure its moving down
                     return True
                 if abs(self.y- new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):
-                    return self.can_kill(new_x, new_y, board)                         
+                    if board[new_x][new_y]:
+                        return self.can_kill(new_x, new_y, board)
+        
+        
+        if board[self.x][self.y].colour == 'W':               
+            if abs(self.y - new_y) < 2 and self.x == new_x and not is_moving_down:
+                return True
+            if abs(self.y- new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):
+                if board[new_x][new_y]:
+                    return self.can_kill(new_x, new_y, board)
+        else:
+            if abs(self.y - new_y) < 2 and self.x == new_x and is_moving_down:
+                return True
+            if abs(self.y- new_y) < 2 and ((self.x + 1) == new_x or (self.x - 1) == new_x):
+                if board[new_x][new_y]:
+                    return self.can_kill(new_x, new_y, board)
         return False
-    
-
-
 
 def main():
         
@@ -293,49 +318,93 @@ def main():
     for i in range(NUM_PAWNS):
         chess.add_piece(Pawn('B', i , 6))
     
-    print(chess.board)
+    chess.pprint()
+    
+                
+            
+     
     while chess.not_game_over():
         while p1_turn:
             print('P1 turn')
             print('Please enter X location of piece you want to move')
-            p1_x_piece = int(input())
+            try:
+                p1_x_piece = int(input())
+            except:
+                print('PLEASE USE AN INT')
+                continue
             print('Please enter Y location of piece you want to move')
-            p1_y_piece = int(input())
-            if chess.board[p1_x_piece][p1_y_piece] != None and chess.board[p1_x_piece][p1_y_piece].colour == 'W':                  
+            try:
+                p1_y_piece = int(input())
+            except:
+                print('PLEASE US AN INT')
+                continue
+            
+            if chess.board[p1_x_piece][p1_y_piece] != None and chess.board[p1_x_piece][p1_y_piece].colour == 'W':
+                available_moves = chess.board[p1_x_piece][p1_y_piece].all_moves(chess.board)
+                print(available_moves)                
                 print('Please enter X location of where to move')
-                p1_x_move = int(input())
+                try:
+                    p1_x_move = int(input())
+                except:
+                    print('PLEASE USE AN INT')
+                    continue
                 print('Please enter Y location of where to move')
-                p1_y_move = int(input())
+                try:
+                    p1_y_move = int(input())
+                except:
+                    print('PLEASE USE AN INT')
+                    continue                    
                 if chess.board[p1_x_piece][p1_y_piece].move(p1_x_move, p1_y_move, chess.board) == False:
                     print('Invalid move try again')
-                    print(chess.board)
+                    chess.pprint()
                 else:
-                    #if chess.board[p1_x_move][p1_y_move].check():
-                        #print('P2 King is in check')
+                    chess.add_move()
+                    if chess.board[p1_x_move][p1_y_move].check(chess.board):
+                        print('P2 King is in check')
                     p1_turn = False
                     p2_turn = True
-                    print(chess.board)
+                    chess.pprint()
+                    
         while p2_turn:
             print('P2 turn')
             print('Please enter X location of piece you want to move')
-            p2_x_piece = int(input())
+            try:
+                p2_x_piece = int(input())
+            except:
+                print('PLEASE USE AN INT')
+                continue
             print('Please enter Y location of piece you want to move')
-            p2_y_piece = int(input())
+            try:
+                p2_y_piece = int(input())
+            except:
+                print('PLEASE US AN INT')
+                continue
             if chess.board[p2_x_piece][p2_y_piece] != None and chess.board[p2_x_piece][p2_y_piece].colour == 'B':
+                available_moves = chess.board[p2_x_piece][p2_y_piece].all_moves(chess.board)
+                print(available_moves)                                
                 print('Please enter X location of where to move')
-                p2_x_move = int(input())
+                try:
+                    p2_x_move = int(input())
+                except:
+                    print('PLEASE USE AN INT')
+                    continue
                 print('Please enter Y location of where to move')
-                p2_y_move = int(input())
+                try:
+                    p2_y_move = int(input())
+                except:
+                    print('PLEASE USE AN INT')
+                    continue    
                 if chess.board[p2_x_piece][p2_y_piece].move(p2_x_move, p2_y_move, chess.board) == False:
                     print('Invalid move try again')
-                    print(chess.board)
+                    chess.pprint()
                 else:
-                    #if chess.board[p2_x_move][p2_y_move].check(chess.board):
-                        #print('P1 King is in check')                    
+                    chess.add_move()
+                    if chess.board[p2_x_move][p2_y_move].check(chess.board):
+                        print('P1 King is in check')                    
                     p1_turn = True
                     p2_turn = False               
                
-        print(chess.board)  
+        chess.pprint()  
     print('game over')
         
     
